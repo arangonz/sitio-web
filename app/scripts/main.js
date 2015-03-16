d83294271b940fc0c6345d11a669e8dec8d11127
francisco <francisco.arangonz@gmail.com>
2015-03-16 22:01:52 +0100

    Practica Terminada (estilos faltantes añadidos y problema de JavaScript resuelto)

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,8 @@
+var google;
 
 function cargaMapa() {
     'use strict';
+
     var varLocation = new google.maps.LatLng(40.507169, -1.588055);
     var varMapoptions = {
         center: varLocation,
