d0ef44b105a7b4fda46002af2c35c2bf275b6d0e
francisco <francisco.arangonz@gmail.com>
2015-03-16 20:35:43 +0100

    Completa, faltan mejoras estilos y error jshint

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,18 @@
-/* jshint devel:true */
-console.log('\'Allo \'Allo!');
+
+function cargaMapa() {
+    'use strict';
+    var varLocation = new google.maps.LatLng(40.507169, -1.588055);
+    var varMapoptions = {
+        center: varLocation,
+        zoom: 15
+    };
+    var varMap;
+    var varMarker = new google.maps.Marker({
+        position: varLocation,
+        map: varMap,
+    });
+    varMap = new google.maps.Map(document.getElementById('googleMap'),
+        varMapoptions);
+    varMarker.setMap(varMap);
+}
+google.maps.event.addDomListener(window, 'load', cargaMapa);
